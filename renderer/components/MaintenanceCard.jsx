64116855c1b96e2b74f3c43ddcea88ca7a32c2b6
Patricio Suarez <102595860+PatoSuar3z@users.noreply.github.com>
2023-05-25 09:49:40 -0400

    Se da estilo a la lista de mantenciones

--- renderer/components/MaintenanceCard.jsx
+++ renderer/components/MaintenanceCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Card, CardBody } from '@material-tailwind/react';
+import { Card, CardBody, Typography } from '@material-tailwind/react';
+
 
 const MaintenanceCard = () => {
   // Información de las solicitudes de mantenimiento
@@ -58,6 +59,7 @@ const MaintenanceCard = () => {
       <Card>
         <CardBody>
           <h2 className="text-lg font-bold mb-4">Pendientes</h2>
+          <Card>
           {pendingRequests.map((request) => (
             <div key={request.id} className="p-4 mb-4 rounded-lg">
               <h3 className="font-semibold">{request.title}</h3>
@@ -70,74 +72,83 @@ const MaintenanceCard = () => {
               </button>
             </div>
           ))}
+          </Card>.
         </CardBody>
       </Card>
 
       <Card>
         <CardBody>
-          <h2 className="text-lg font-bold mb-4">En curso</h2>
+        <h2 className="text-lg font-bold mb-4">En curso</h2>
+        <Card>
           {ongoingRequests.map((request) => (
             <div key={request.id} className="p-4 mb-4 rounded-lg">
               <h3 className="font-semibold">{request.title}</h3>
               <p className="text-sm">{request.description}</p>
               <button
-                className="bg-green-500 text-white px-2 py-1 mt-2 rounded"
+                className="bg-blue-500  text-white px-2 py-1 mt-2 rounded"
                 onClick={() => changeStatus(request.id, 'Finalizada')}
               >
                 Marcar como finalizada
               </button>
             </div>
           ))}
+          </Card>
         </CardBody>
       </Card>
 
       <div className="col-span-2">
-        <h2 className="text-lg font-bold mb-4">Finalizadas</h2>
-        <div className="flex items-center mb-4">
-          <label htmlFor="search" className="mr-2">
-            Buscar por ID:
-          </label>
-          <input
-            type="text"
-            id="search"
-            className="border border-gray-300 rounded px-2 py-1"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-        </div>
-        <div className="overflow-x-auto">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  ID
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Solicitud
-                </th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Descripción
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {filteredCompletedRequests.map((request) => (
-                <tr key={request.id}>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900">{request.id}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900">{request.title}</div>
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <div className="text-sm text-gray-900">{request.description}</div>
-                  </td>
+        <div className="bg-white rounded-lg shadow-md p-6">
+          <h2 className="text-lg font-bold mb-4 text-gray-950">Finalizadas</h2>
+          <div className="flex items-center mb-4">
+            <label htmlFor="search" className="mr-2 text-gray-950">
+              Buscar por ID:
+            </label>
+            <input
+              type="text"
+              id="search"
+              className="border border-gray-300 rounded px-2 py-1"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
+          <div className="overflow-x-auto">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    ID
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Solicitud
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Descripción
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {filteredCompletedRequests.map((request) => (
+                  <tr key={request.id}>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{request.id}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{request.title}</div>
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap">
+                      <div className="text-sm text-gray-900">{request.description}</div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
+
+      {/* Utilizar esta Card */}
+
+
     </div>
   );
 };
