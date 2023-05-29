5850d6dbcdca7592293b7ac4a03100bd907fac9a
PatoSuar3z <pat.suarez@duocuc.cl>
2023-05-29 18:42:10 -0400

    Se Agrega Componentene BooksAlphillia y se listan libros directamente desde la API

---
+++ renderer/components/BooksAlphillia.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { PencilIcon } from "@heroicons/react/24/solid";
+import {
+    Typography,
+    Avatar,
+    IconButton,
+} from "@material-tailwind/react";
+
+const BooksAlphillia = ({ alphilliaBooks }) => {
+    return (
+        <>
+            {alphilliaBooks.map((book) => (
+                <tr key={book.id}>
+                    <td>
+                        <div className="flex items-center gap-3">
+                            <Avatar
+                                src={book.foto}
+                                width={60}
+                                alt={book.titulo}
+                                size="md"
+                                className="border border-blue-gray-50 bg-blue-gray-50/50 object-contain p-1"
+                            />
+                            <Typography variant="small" color="blue-gray" className="font-bold">
+                                {book.titulo}
+                            </Typography>
+                        </div>
+                    </td>
+                    <td >
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {book.isbn}
+                        </Typography>
+                    </td>
+                    <td >
+                        <Typography variant="small" color="blue-gray" className="font-normal">
+                            {book.stock}/100
+                        </Typography>
+                    </td>
+                    <td >
+                        <div className="w-max">
+                            <Typography variant="small" color="blue-gray" className="font-normal">
+                                ${book.precio}
+                            </Typography>
+                        </div>
+                    </td>
+                    <td >
+
+                        <PencilIcon className="h-4 w-4" />
+
+                    </td>
+                </tr>
+            ))}
+        </>
+    )
+}
+
+export default BooksAlphillia
--- renderer/components/DeliveryTable.jsx
+++ renderer/components/DeliveryTable.jsx
@@ -1,74 +1,85 @@
-import { useEffect, useState } from 'react';
-import { Card, CardBody, Typography, Button, Input } from '@material-tailwind/react';
-import { PencilIcon, TruckIcon, SearchIcon } from '@heroicons/react/outline';
-import axios from 'axios';
+import { useEffect, useState } from 'react'
+import {
+    Card,
+    CardBody,
+    Typography,
+    Button,
+    Input,
+} from '@material-tailwind/react'
+import { PencilIcon, TruckIcon, SearchIcon } from '@heroicons/react/outline'
+import axios from 'axios'
 
 export default function DeliveryTable() {
-  const [shipping, setShipping] = useState([]);
-  const [selectedEstado, setSelectedEstado] = useState('');
-  const [idToUpdate, setIdToUpdate] = useState('');
-  const [completedShipments, setCompletedShipments] = useState([]);
-  const [searchText, setSearchText] = useState('');
-  const [filteredShipments, setFilteredShipments] = useState([]);
+    const [shipping, setShipping] = useState([])
+    const [selectedEstado, setSelectedEstado] = useState('')
+    const [idToUpdate, setIdToUpdate] = useState('')
+    const [completedShipments, setCompletedShipments] = useState([])
+    const [searchText, setSearchText] = useState('')
+    const [filteredShipments, setFilteredShipments] = useState([])
 
-  const handleUpdate = () => {
-    console.log(idToUpdate);
-    const updatedData = {
-      estado: selectedEstado
-    };
+    const handleUpdate = () => {
+        console.log(idToUpdate)
+        const updatedData = {
+            estado: selectedEstado,
+        }
 
-    axios.put(`http://localhost:9001/envios/${idToUpdate}`, updatedData)
-      .then(response => {
-        console.log('Cambio de estado guardado exitosamente');
-      })
-      .catch(error => {
-        console.error('Error en la solicitud PUT:', error);
-        console.log(updatedData);
-        console.log(idToUpdate);
-      });
-  };
+        axios
+            .put(`http://localhost:9001/envios/${idToUpdate}`, updatedData)
+            .then((response) => {
+                console.log('Cambio de estado guardado exitosamente')
+            })
+            .catch((error) => {
+                console.error('Error en la solicitud PUT:', error)
+                console.log(updatedData)
+                console.log(idToUpdate)
+            })
+    }
 
-  useEffect(() => {
-    const fetchShippingData = () => {
-      fetch('http://localhost:9001/envios')
-        .then(res => res.json())
-        .then(res => setShipping(res))
-        .catch(error => {
-          console.error('Error al obtener los datos de envío:', error);
-        });
-    };
+    useEffect(() => {
+        const fetchShippingData = () => {
+            fetch('http://localhost:9001/envios')
+                .then((res) => res.json())
+                .then((res) => setShipping(res))
+                .catch((error) => {
+                    console.error('Error al obtener los datos de envío:', error)
+                })
+        }
 
-    // Fetch shipping data initially
-    fetchShippingData();
+        // Fetch shipping data initially
+        fetchShippingData()
 
-    // Fetch shipping data every 10 seconds (adjust the interval as needed)
-    const interval = setInterval(fetchShippingData, 10000);
+        // Fetch shipping data every 10 seconds (adjust the interval as needed)
+        const interval = setInterval(fetchShippingData, 5000)
 
-    // Clean up the interval when the component unmounts
-    return () => clearInterval(interval);
-  }, []);
+        // Clean up the interval when the component unmounts
+        return () => clearInterval(interval)
+    }, [])
 
-  useEffect(() => {
-    const completedShipments = shipping.filter(shipment => shipment.estado === 'Entregado');
-    setCompletedShipments(completedShipments);
-  }, [shipping]);
+    useEffect(() => {
+        const completedShipments = shipping.filter(
+            (shipment) => shipment.estado === 'Entregado'
+        )
+        setCompletedShipments(completedShipments)
+    }, [shipping])
 
-  useEffect(() => {
-    const filteredShipments = completedShipments.filter(shipment =>
-      shipment.id_envio.includes(searchText) || shipment.id_usuario.includes(searchText)
-    );
-    setFilteredShipments(filteredShipments);
-  }, [completedShipments, searchText]);
+    useEffect(() => {
+        const filteredShipments = completedShipments.filter(
+            (shipment) =>
+                shipment.id_envio.includes(searchText) ||
+                shipment.id_usuario.includes(searchText)
+        )
+        setFilteredShipments(filteredShipments)
+    }, [completedShipments, searchText])
 
-  const TABLE_HEAD = [
-    'Codigo Rastreo',
-    'Fecha',
-    'Estado',
-    'Destino',
-    'Valor',
-    'ID Cliente',
-    'Editar Estado'
-  ];
+    const TABLE_HEAD = [
+        'Codigo Rastreo',
+        'Fecha',
+        'Estado',
+        'Destino',
+        'Valor',
+        'ID Cliente',
+        'Editar Estado',
+    ]
 
     return (
         <div className="grid grid-cols-2 gap-4">
@@ -96,6 +107,13 @@ export default function DeliveryTable() {
                                     className="mt-1"
                                 >
                                     Estado: {shipment.estado}
+                                </Typography>
+                                <Typography
+                                    variant="small"
+                                    color="blue-gray"
+                                    className="mt-1"
+                                >
+                                    ID: {shipment.id_envio}
                                 </Typography>
                                 <Button
                                     color="blue"
@@ -158,44 +176,69 @@ export default function DeliveryTable() {
                 </CardBody>
             </Card>
 
-            <Card>
-        <CardBody>
-          <div className="flex items-center mb-4">
-            <h2 className="text-lg font-bold mr-4">Envíos Finalizados</h2>
-            <div className="flex items-center">
-              
-              <Input
-                type="text"
-                placeholder="Buscar por ID de Envío o ID de Usuario"
-                value={searchText}
-                onChange={e => setSearchText(e.target.value)}
-              />
+            <div className="col-span-2">
+                <div className="bg-white rounded-lg shadow-md p-6">
+                    <h2 className="text-lg font-bold mb-4 text-gray-950">
+                        Finalizadas
+                    </h2>
+                    <div className="flex items-center mb-4">
+                        <label htmlFor="search" className="mr-2 text-gray-950">
+                            Buscar por ID:
+                        </label>
+                        <input
+                            type="text"
+                            id="search"
+                            className="border border-gray-300 rounded px-2 py-1"
+                        />
+                    </div>
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full divide-y divide-gray-200">
+                            <thead className="bg-gray-50">
+                                <tr>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        ID Envio
+                                    </th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Destino
+                                    </th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Valor
+                                    </th>
+                                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        ID Usuario
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="bg-white divide-y divide-gray-200">
+                                {filteredShipments.map((shipment) => (
+                                    <tr key={shipment.id_envio}>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm text-gray-900">
+                                                {shipment.id_envio}
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm text-gray-900">
+                                                {shipment.destino}
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm text-gray-900">
+                                                {shipment.valor}
+                                            </div>
+                                        </td>
+                                        <td className="px-6 py-4 whitespace-nowrap">
+                                            <div className="text-sm text-gray-900">
+                                                {shipment.id_usuario}
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="overflow-x-auto">
-            <table className="min-w-full bg-white">
-              <thead>
-                <tr>
-                  <th className="px-4 py-2">ID Envío</th>
-                  <th className="px-4 py-2">Destino</th>
-                  <th className="px-4 py-2">Valor</th>
-                  <th className="px-4 py-2">ID Usuario</th>
-                </tr>
-              </thead>
-              <tbody>
-                {filteredShipments.map(shipment => (
-                  <tr key={shipment.id_envio}>
-                    <td className="border px-4 py-2">{shipment.id_envio}</td>
-                    <td className="border px-4 py-2">{shipment.destino}</td>
-                    <td className="border px-4 py-2">{shipment.valor}</td>
-                    <td className="border px-4 py-2">{shipment.id_usuario}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </CardBody>
-      </Card>
         </div>
     )
 }
