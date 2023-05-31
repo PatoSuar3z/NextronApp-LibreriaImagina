2040c7dff76ec2d7994356886407d949b6a09c66
PatoSuar3z <pat.suarez@duocuc.cl>
2023-05-31 00:40:49 -0400

    Se edita para implementar el Darkmode

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from "@material-tailwind/react";
+import { DarkProvider } from '../context/darkMode'; 
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
-      <Component {...pageProps} />
+      <DarkProvider>
+        <Component {...pageProps} />
+      </DarkProvider>
     </ThemeProvider>
   );
 }
