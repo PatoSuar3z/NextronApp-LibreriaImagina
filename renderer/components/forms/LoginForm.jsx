015c2bddc8c88b1d1cbb938248b60565c5927ef2
PatoSuar3z <pat.suarez@duocuc.cl>
2023-06-05 23:42:45 -0400

    Edit : color error

--- renderer/components/forms/LoginForm.jsx
+++ renderer/components/forms/LoginForm.jsx
@@ -92,8 +92,9 @@ const LoginForm = () => {
         value={values.correo}
         onChange={handleChange}
         onBlur={handleBlur}
+        style={{ color: 'black' }}
       />
-      {touched.correo && errors.correo && <p>{errors.correo}</p>}
+      {touched.correo && errors.correo && <p style={{ color: '#664040' }}>{errors.correo}</p>}
       <div className="relative">
         <input
           className="text-sm p-2 rounded-xl border w-full"
@@ -103,12 +104,14 @@ const LoginForm = () => {
           value={values.password}
           onChange={handleChange}
           onBlur={handleBlur}
+          style={{ color: '#664040' }}
         />
         <svg
           className={
+            
             errors.password && touched.password
               ? 'bi bi-eye absolute top-1/4 right-3 -translate-y-1/2  cursor-pointer'
-              : 'bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer'
+              : 'bi bi-eye absolute top-1/2 right-3 -translate-y-1/2 cursor-pointer '
           }
           xmlns="http://www.w3.org/2000/svg"
           width="16"
@@ -116,19 +119,20 @@ const LoginForm = () => {
           fill="gray"
           viewBox="0 0 16 16"
           onClick={toggleShowPassword}
+          
         >
           <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
           <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
         </svg>
         {touched.password && errors.password && (
-          <p className="mt-3">{errors.password}</p>
+          <p className="mt-3" style={{ color: '#664040' }}>{errors.password}</p>
         )}
       </div>
       <button
         className="bg-[#664040] rounded-xl text-white py-2 hover:scale-105 duration-300"
         type="submit"
       >
-        Login
+        Iniciar Sesión
       </button>
       <Error isOpen={isErrorModalOpen} onClose={handleCloseErrorModal}>
         {error}
