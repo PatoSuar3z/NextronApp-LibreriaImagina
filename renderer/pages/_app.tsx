779a0c3159fd177f9dea97c9a73ab797228be35b
PatoSuar3z <pat.suarez@duocuc.cl>
2023-06-05 23:51:29 -0400

    Update :

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from "@material-tailwind/react";
-import { DarkProvider } from '../context/darkMode'; 
+import { DarkProvider } from '../context/darkMode';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
   return (
+   
     <ThemeProvider>
       <DarkProvider>
         <Component {...pageProps} />
